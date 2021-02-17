215d7aeb31929c5983723ead39399be85d029734
hjlee0320 <kitty0345@gmail.com>
2021-02-17 15:25:20 +0900

    메인화면 레이아웃 완성

--- app/src/main/java/com/example/anzela/MainActivity.kt
+++ app/src/main/java/com/example/anzela/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.anzela
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
