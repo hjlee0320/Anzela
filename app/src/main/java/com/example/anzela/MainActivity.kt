458eb348f710f3ddd92a45293c12a00cdff4142a
hjlee0320 <kitty0345@gmail.com>
2021-02-23 22:50:39 +0900

    main backend 1

--- app/src/main/java/com/example/anzela/MainActivity.kt
+++ app/src/main/java/com/example/anzela/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.anzela
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        firstInit()
     }
 
+    fun firstInit() {
+        btn_menu.setOnClickListener(this)
+        btn_noti.setOnClickListener(this)
+        btn_myInfo.setOnClickListener(this)
+        my_location.setOnClickListener(this)
+        btn_ridingList.setOnClickListener(this)
+
+        tv_uInfo.setText(resources.getText(R.string.u_info, "do"))
+
+
+    }
+
+
     override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+
     }
 }
